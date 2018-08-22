95261093de0c8181879e090cd592f049a2ea6801
Pablo <pablo.um00@gmail.com>
2018-08-21 23:11:42 -0500

    Challenge artists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'artist', to: "artist#index"
+  get 'artist/:id', to: "artist#albums", as: "artist_id"
+  root 'artist#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
